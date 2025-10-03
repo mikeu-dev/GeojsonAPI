306df09ee531bb4f60d61356b897d8484d528214
Riki Ruswandi <riki@pratamatechsolution.co.id>
2025-10-03 19:58:15 +0700

    feat:controllers auth

---
+++ Controllers/Auth/AuthController.cs
@@ -0,0 +1,42 @@
+﻿using GeojsonAPI.Models.User;
+using GeojsonAPI.Service.Auth;
+using Microsoft.AspNetCore.Mvc;
+using RegisterRequest = GeojsonAPI.Requests.Auth.RegisterRequest;
+using LoginRequest = GeojsonAPI.Requests.Auth.LoginRequest;
+
+namespace GeojsonAPI.Controllers.Auth
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly AuthService _authService;
+
+        public AuthController(AuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public ActionResult<User> Register([FromBody] RegisterRequest req)
+        {
+            try
+            {
+                var user = _authService.Register(req.Username, req.Email, req.Password);
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("login")]
+        public ActionResult Login([FromBody] LoginRequest req)
+        {
+            if (_authService.Login(req.Username, req.Password))
+                return Ok("Login berhasil");
+            return Unauthorized("Username atau password salah");
+        }
+    }
+}
