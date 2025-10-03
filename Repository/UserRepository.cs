3b0771365e8cd4ef89408adc8b367af8627d975f
Riki Ruswandi <riki@pratamatechsolution.co.id>
2025-10-03 19:59:36 +0700

    feat: repository user

---
+++ Repository/UserRepository.cs
@@ -0,0 +1,22 @@
+﻿using GeojsonAPI.Models.User;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace GeojsonAPI.Repository
+{
+    public class UserRepository
+    {
+        private readonly IMongoCollection<User> _users;
+
+        public UserRepository(IOptions<Settings> settings)
+        {
+            var client = new MongoClient(settings.Value.ConnectionString);
+            var database = client.GetDatabase(settings.Value.DatabaseName);
+            _users = database.GetCollection<User>("Users");
+        }
+
+        public User GetById(string id) => _users.Find(u => u.Id == id).FirstOrDefault();
+        public User GetByUsername(string username) => _users.Find(u => u.Username == username).FirstOrDefault();
+        public void Create(User user) => _users.InsertOne(user);
+    }
+}
