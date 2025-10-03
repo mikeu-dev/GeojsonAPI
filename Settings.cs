6054a07b9028ed67df904956a5624b2dd93410a6
Riki Ruswandi <riki@pratamatechsolution.co.id>
2025-10-03 20:00:41 +0700

    feat:settings class

---
+++ Settings.cs
@@ -0,0 +1,8 @@
+﻿namespace GeojsonAPI
+{
+    public class Settings
+    {
+        public required string ConnectionString { get; set; }
+        public string DatabaseName { get; set; } = string.Empty;
+    }
+}
