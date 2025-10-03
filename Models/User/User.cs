862f48ce09f0441823108e26d739dcba3722e267
Riki Ruswandi <riki@pratamatechsolution.co.id>
2025-10-03 19:58:32 +0700

    feat:model user

---
+++ Models/User/User.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace GeojsonAPI.Models.User
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public required string Id { get; set; }
+
+        [BsonElement("username")]
+        public required string Username { get; set; }
+
+        [BsonElement("email")]
+        public required string Email { get; set; }
+
+        [BsonElement("passwordHash")]
+        public required string PasswordHash { get; set; }
+
+        [BsonElement("role")]
+        public string Role { get; set; } = "User";
+    }
+}
