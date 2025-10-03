278ae6312c51522f509c1c6769ec63f8839bd88f
Riki Ruswandi <riki@pratamatechsolution.co.id>
2025-10-03 19:59:48 +0700

    feat: request auth

---
+++ Requests/Auth/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace GeojsonAPI.Requests.Auth
+{
+    public class LoginRequest
+    {
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+    }
+}
---
+++ Requests/Auth/RegisterRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace GeojsonAPI.Requests.Auth
+{
+    public class RegisterRequest
+    {
+        public required string Username { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+    }
+}
