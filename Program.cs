b602dad638475463570d95fd2989eca65370e123
myqnrrhmn <romanovthewriter@gmail.com>
2025-10-16 14:32:03 +0700

    feat:modified

--- Program.cs
+++ Program.cs
@@ -1,11 +1,87 @@
+using GeojsonAPI;
+using GeojsonAPI.Filters;
+using GeojsonAPI.Repository;
+using GeojsonAPI.Service.Auth;
+using GeojsonAPI.Service.GeoJSON;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.Configure<Settings>(
+    builder.Configuration.GetSection("Settings"));
 // Add services to the container.
-
+builder.Services.AddScoped<UserRepository>();
+builder.Services.AddScoped<GeoJSONService>();
+builder.Services.AddScoped<AuthService>();
+builder.Services.AddScoped<GeoJSONRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var jwtSettings = builder.Configuration.GetSection("Jwt");
+#pragma warning disable CS8604 // Possible null reference argument.
+var key = Encoding.ASCII.GetBytes(s: jwtSettings["Key"]);
+#pragma warning restore CS8604 // Possible null reference argument.
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtSettings["Issuer"],
+        ValidAudience = jwtSettings["Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "GeojsonAPI",
+        Version = "v1"
+    });
+
+    c.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Description = "Masukkan JWT token tanpa kata 'Bearer '",
+        Name = "Authorization",
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT"
+    });
+
+    c.OperationFilter<AuthorizeCheckOperationFilter>();
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
+
 
 var app = builder.Build();
 
@@ -17,7 +93,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
--- Requests/Auth/LoginRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GeojsonAPI.Requests.Auth
-{
-    public class LoginRequest
-    {
-        public required string Username { get; set; }
-        public required string Password { get; set; }
-    }
-}
